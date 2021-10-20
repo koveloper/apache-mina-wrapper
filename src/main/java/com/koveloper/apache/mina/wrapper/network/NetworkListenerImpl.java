ccd6e8faa4fbcf76c8c7cdc414ef0b576e788ffd
koveloper <koveloper@gmail.com>
2021-10-20 11:46:53 +0300

    Add NetworkListener interface adapter

---
+++ src/main/java/com/koveloper/apache/mina/wrapper/network/NetworkListenerImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.koveloper.apache.mina.wrapper.network;
+
+/**
+ *
+ * @author koban
+ */
+public class NetworkListenerImpl implements NetworkListener{
+    
+    @Override
+    public void error(Object iptr__, Object error) {}
+    
+    @Override
+    public void connected(Object iptr__, Object src) {}
+    
+    @Override
+    public void disconnected(Object iptr__, Object src) {}
+    
+    @Override
+    public void finished(Object iptr__) {}
+    
+    @Override
+    public void dataReceived(Object iptr__, NetworkConnectionData data) {}
+}
